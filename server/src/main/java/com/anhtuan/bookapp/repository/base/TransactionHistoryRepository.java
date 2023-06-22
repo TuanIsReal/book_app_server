0ff2739ee58d2f2129ad468f5f55e86d63a87e1b
LAPTOP-HV8QENET\anhtu <tuan.le1@ntq-solution.com.vn>
2023-06-22 22:32:38 +0700

    update code 6

--- server/src/main/java/com/anhtuan/bookapp/repository/base/TransactionHistoryRepository.java
+++ server/src/main/java/com/anhtuan/bookapp/repository/base/TransactionHistoryRepository.java
@@ -1,10 +1,11 @@
 package com.anhtuan.bookapp.repository.base;
 
 import com.anhtuan.bookapp.domain.TransactionHistory;
+import com.anhtuan.bookapp.repository.customize.TransactionHistoryCustomizeRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-public interface TransactionHistoryRepository extends MongoRepository<TransactionHistory, String> {
-    List<TransactionHistory> getTransactionHistoriesByUserId(String userId);
+public interface TransactionHistoryRepository extends MongoRepository<TransactionHistory, String>, TransactionHistoryCustomizeRepository {
+    List<TransactionHistory> getTransactionHistoriesByUserIdOrderByTransactionTimeDesc(String userId);
 }
