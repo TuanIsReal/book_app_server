d74ab174d180c4a73dfd31b5d7295651670bc526
Tuan Le Anh <tuan.le1@ntq-solution.com.vn>
2023-11-07 09:25:55 +0700

    20231107_TuanLA_Deploy_AWS_Sever

--- server/src/main/java/com/anhtuan/bookapp/config/Constant.java
+++ server/src/main/java/com/anhtuan/bookapp/config/Constant.java
@@ -7,8 +7,6 @@ public class Constant {
     public static final String CHAPTER_TEXT_STORAGE_PATH = "C:/chapter/";
     public static final String JPG = ".jpg";
     public static final String TXT = ".txt";
-    public static final String HTTP = "http://";
-    public static final String IP_SERVER = "192.168.1.73";
     public static final String BOOK_IMAGE_STORAGE_PATH_RESPONSE = "/book_image/";
     public static final String THUMBNAIL_STORAGE_PATH_RESPONSE = "/thumbnail/";
     public static final String AVATAR_IMAGE_STORAGE_PATH_RESPONSE = "/avatar_image/";
