121e690348b2abdbbd8f654d54bca204c41643f2
Tuan Le Anh <tuan.le1@ntq-solution.com.vn>
2023-11-08 15:00:33 +0700

    20231108_TuanLA_Test_Security

--- server/src/main/java/com/anhtuan/bookapp/BookAppServerApplication.java
+++ server/src/main/java/com/anhtuan/bookapp/BookAppServerApplication.java
@@ -1,11 +1,5 @@
 package com.anhtuan.bookapp;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -18,11 +12,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @AllArgsConstructor
 @EnableConfigurationProperties
 @EnableScheduling
-@SecurityScheme(name = "scheme security",type = SecuritySchemeType.HTTP,bearerFormat = "JWT",scheme ="Bearer",in = SecuritySchemeIn.HEADER)
-@OpenAPIDefinition(info = @Info(title = "Book App API",
-		version = "3.0",
-		description = "Book App API"),
-		security ={@SecurityRequirement(name = "scheme security")})
 public class BookAppServerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
